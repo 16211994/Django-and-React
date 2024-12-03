8021a1e95687a1ca7d50eadaa5da504b11f7681c
16211994 <cakek433@gmail.com>
2024-12-03 23:22:23 +0300

    Change by adding curly brace to import protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Register from "./pages/Register"
-import Home from "./pages/Home"
+import { Home } from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import Login from "./pages/login" 
-import ProtectedRoute from "./components/ProtectedRoute"
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
 
 function Logout() {
   localStorage.clear()
